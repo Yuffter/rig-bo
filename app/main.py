cc5b4c1bb155d0dc9fe5e1b942ad21b6f5ffa202
Yuffter <89680576+Yuffter@users.noreply.github.com>
2024-06-22 09:54:14 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from server import server_thread
 from discord import app_commands
+import random
 
 load_dotenv()
 
@@ -47,9 +48,21 @@ async def on_guild_channel_pins_update(channel, last_pin):
     await destination_channel.send(
         f'#{channel.name} において、メッセージがピン止めされました\n{latest_pin.jump_url}\n{latest_pin.content}')"""
     
-@tree.command(name="test",description="テストコマンドです。")
-async def test_command(interaction: discord.Interaction):
-    await interaction.response.send_message("てすと！",ephemeral=True)#ephemeral=True→「これらはあなただけに表示されています」
+@tree.command(name="omikuji",description="おみくじが引けます")
+async def do_omikuji(interaction: discord.Interaction):
+    rnd = random.randint(0,100)
+    probability = [1, 16, 5, 7, 35, 6, 29, 1]
+    tier = ["大大吉","大吉","中吉","小吉","吉","末吉","凶","大凶"]
+
+    for i in range(len(probability)):
+        if rnd < probability[i]:
+            await interaction.response.send_message(f"{interaction.user.name}の今日の運勢は...\n**{tier[i]}** です")
+            break
+        else:
+            rnd -= probability[i]
+
+    
+    #await interaction.response.send_message("てすと！",ephemeral=True)#ephemeral=True→「これらはあなただけに表示されています」
     
 server_thread()
 client.run(TOKEN)
