5963df7d0160e42826227e07f6f1889510b420c3
Yuffter <89680576+Yuffter@users.noreply.github.com>
2024-06-22 09:09:56 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from server import server_thread
+from discord import app_commands
 
 load_dotenv()
 
@@ -11,10 +12,12 @@ intents.message_content = True
 intents.voice_states = True
 intents.guilds = True
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f'ログインしました: {client.user}')
+    await tree.sync()#スラッシュコマンドを同期
 
 @client.event
 async def on_message(message):
@@ -43,6 +46,10 @@ async def on_guild_channel_pins_update(channel, last_pin):
     """destination_channel = client.get_channel(1250813996995575868)
     await destination_channel.send(
         f'#{channel.name} において、メッセージがピン止めされました\n{latest_pin.jump_url}\n{latest_pin.content}')"""
+    
+@tree.command(name="test",description="テストコマンドです。")
+async def test_command(interaction: discord.Interaction):
+    await interaction.response.send_message("てすと！",ephemeral=True)#ephemeral=True→「これらはあなただけに表示されています」
     
 server_thread()
 client.run(TOKEN)
