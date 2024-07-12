8bd44a09d1720f0984fc80c1b43658f9264c4d91
Yuffter <89680576+Yuffter@users.noreply.github.com>
2024-07-13 08:31:21 +0900

    main.py の更新

--- app/main.py
+++ app/main.py
@@ -56,8 +56,8 @@ async def on_guild_channel_pins_update(channel, last_pin):
 async def do_omikuji(interaction: discord.Interaction):
     rnd = random.randint(0, 100)
     probability = [1, 16, 9, 12, 35, 12, 14, 1]
-    probability = [100,0,0,0,0,0,0,0]
-    #tier = ["大大吉", "大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"]
+    #probability = [100,0,0,0,0,0,0,0]
+    tier = ["大大吉", "大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"]
     imageUrl = [
         "https://pbs.twimg.com/media/FlRp3-2aMAAJjQ8.jpg",
         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimpKtspceL47HWV8CIjCG83OLzaXss2VrjPQt65pfItad0LzQVB13lABAZ8zvViixYeemTkX9O3F2W9vfmDrv2u00nRzGmVD4OIj81oM6zOk84edl8Loj2BvpLIkT4TgWCiPJr4YMSzQZE/s400/omikuji_daikichi.png",
